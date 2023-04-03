a82f86b894d562d1055424cf143d2f00232335cc
Aldo Fiorito <aldo.fiorito97@gmail.com>
2023-04-03 19:19:23 +0200

    Moved logic into resume

--- app/src/main/java/com/example/teamake/HomepageActivity.java
+++ app/src/main/java/com/example/teamake/HomepageActivity.java
@@ -146,6 +146,12 @@ public class HomepageActivity extends AppCompatActivity  {
 
         matchesPendingView = findViewById(R.id.pendingMatches);
         listMatchPending = new ArrayList<>();
+        matchesPendingView.setHasFixedSize(true);
+        //listMatchPending.add(new MatchItem("xxx", R.drawable.baseline_sports_basketball_24, "basket", "22", 0, 0, R.drawable.baseline_check_24));
+        matchesPendingView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        mAdapter = new MatchesAdapter(listMatchPending);
+        matchesPendingView.setAdapter(mAdapter);
 
         permissionCheckRead = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE);
 
@@ -202,11 +208,9 @@ public class HomepageActivity extends AppCompatActivity  {
             }
 
 
-
-
-            buildRecyclerView();
+            //buildRecyclerView();
             //LinkAdapterToList();
-            //ManageOnClickMatch();
+            ManageOnClickMatch();
 
             logoutBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -320,6 +324,8 @@ public class HomepageActivity extends AppCompatActivity  {
                                     String notificationID = document.getId();
                                     Log.i(TAG, "DOC ID:"+notificationID + " => ADDING ID MATCH: " + localIdMatch);
                                     matchesToDisplayDb.add(localIdMatch);
+
+                                    //match-notify
                                     linkingNotificationMatches.put(localIdMatch,notificationID);
                                     System.out.println("Size matches:"+ matchesToDisplayDb.size());
 
@@ -375,12 +381,14 @@ public class HomepageActivity extends AppCompatActivity  {
                                                 imageToUse = R.drawable.baseline_group_add_24;
                                         }
 
-                                        MatchItem MI = new MatchItem(document.getId(), imageToUse, sport, date, -1, -1, R.drawable.baseline_check_24);
+                                        //MatchItem MI = new MatchItem(document.getId(), imageToUse, sport, date, -1, -1, R.drawable.baseline_check_24);
+                                        MatchItem MI = new MatchItem(document.getId(), imageToUse, document.getId(), date, -1, -1, R.drawable.baseline_check_24);
                                         listMatchPending.add(MI);
 
                                     }
                                 }
-                                LinkAdapterToList();
+                                //LinkAdapterToList();
+                                mAdapter.notifyDataSetChanged();
                             } else {
                                 Log.d(TAG, "Error getting documents: ", task.getException());
                             }
@@ -392,12 +400,7 @@ public class HomepageActivity extends AppCompatActivity  {
 
     protected void LinkAdapterToList() {
         Log.i(TAG,"SIZE Pending matches: " + listMatchPending.size());
-
-        matchesPendingView.setHasFixedSize(true);
-        //listMatchPending.add(new MatchItem("xxx", R.drawable.baseline_sports_basketball_24, "basket", "22", 0, 0, R.drawable.baseline_check_24));
-        mAdapter = new MatchesAdapter(listMatchPending);
-        matchesPendingView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        matchesPendingView.setAdapter(mAdapter);
+        //mAdapter.notifyDataSetChanged();
         ManageOnClickMatch();
     }
 
@@ -481,7 +484,7 @@ public class HomepageActivity extends AppCompatActivity  {
                         Log.d(TAG, "Notification"+ notificationToRemove +" successfully deleted!");
                         linkingNotificationMatches.remove(matchID);
                         listMatchPending.remove(position);
-                        mAdapter.notifyDataSetChanged();
+                        mAdapter.notifyItemChanged(position);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -493,6 +496,126 @@ public class HomepageActivity extends AppCompatActivity  {
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"ON START");
+        //LinkAdapterToList();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG,"ON PAUSE");
+        ArrayList<String> newMatches = new ArrayList<>();
+        Notifications
+                .whereEqualTo("UID", userLogged.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                String localUID = document.getString("UID");
+                                String localStatus = document.getString("status");
+                                String localNotification = document.getId();
+                                String localMatch = document.getString("id_match");
+
+                                if(!localUID.equals(userLogged.getUid()) && !localStatus.equals("unread")) continue;
+
+                                if(!linkingNotificationMatches.containsKey(localMatch)) {
+                                    Log.i(TAG,"ADDING NEW Match"+localMatch);
+                                    Log.i(TAG,"ADDING NEW Notification"+document.getString("id_match"));
+
+                                    linkingNotificationMatches.put(localMatch,localNotification);
+                                    newMatches.add(document.getString("id_match"));
+                                }
+                            }
+
+                        }
+                        Log.d(TAG, "new matches: " + newMatches);
+                        Log.i(TAG, String.valueOf(newMatches.size()));
+                        if(!newMatches.isEmpty()) {
+                            Log.i(TAG, "calling populate v2 for : " + userLogged.getUid());
+                            PopulateRecyclerView(newMatches);
+                        }
+                        else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+
+                    }
+                });
+        }
+
+
+        /*
+    protected  void PopulateRecyclerView(ArrayList<String> matchesToDisplayDb){
+
+        Log.i(TAG,"DISPLAYING ALL PENDING MATCHES  V2 FOR: "+userLogged.getUid());
+        Log.i(TAG, String.valueOf(matchesToDisplayDb.size()));
+        if(!matchesToDisplayDb.isEmpty()) {
+            Matches
+                    .whereIn(FieldPath.documentId(), matchesToDisplayDb)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                    String status = document.getString("Status");
+                                    HashMap<String,ArrayList<String>> players;
+                                    players = ((HashMap<String,ArrayList<String>> ) document.get("Players"));
+                                    boolean isPlayerInvited = false;
+
+                                    if (players.containsKey(userLogged.getUid())) isPlayerInvited = true;
+                                    if (status.equals("Pending") && isPlayerInvited) {
+
+                                        Log.i(TAG,"Found NEW matchID: "+document.getId());
+
+                                        String date = document.getString("Date");
+                                        String sport = document.getString("Sport");
+                                        Integer imageToUse;
+                                        switch (sport) {
+                                            case "Tennis":
+                                                imageToUse = R.drawable.baseline_sports_tennis_24;
+                                                break;
+                                            case "Soccer":
+                                                imageToUse = R.drawable.baseline_sports_soccer_24;
+                                                break;
+                                            case "Basket":
+                                                imageToUse = R.drawable.baseline_sports_basketball_24;
+                                                break;
+                                            default:
+                                                imageToUse = R.drawable.baseline_group_add_24;
+                                        }
+
+                                        //MatchItem MI = new MatchItem(document.getId(), imageToUse, sport, date, -1, -1, R.drawable.baseline_check_24);
+                                        MatchItem MI = new MatchItem(document.getId(), imageToUse, document.getId(), date, -1, -1, R.drawable.baseline_check_24);
+                                        listMatchPending.add(MI);
+
+                                    }
+                                }
+                                mAdapter.notifyDataSetChanged();
+                            } else {
+                                Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+                        }
+
+                    });
+        }
+    }
+
+         */
+
+
+
 
 }
 
